1f14a08d0e419a75e368e5d55e44e33c14908599
Pedro Ribeiro <pedro.ribeiro@york.ac.uk>
2020-12-02 18:50:47 +0000

    Update upload.sh
    Changing upload script to include branch name parameter. This is needed to compile a version of the RoboSim validator compatible with the comp family of branches.

--- upload.sh
+++ upload.sh
@@ -5,15 +5,27 @@ url=ahm504@sftp.york.ac.uk
 file=$(ls $dir/features | grep -m 1 jar)
 version=${file#*_}
 version=${version%.jar}
+
+# Use the branch name to choose the name of the branch. This assumes
+# no branch of name 'update' will ever be used.
+if [[ $WERCKER_GIT_BRANCH = master ]];
+then
+  update=update
+else
+  update=$WERCKER_GIT_BRANCH
+fi
+
 if [[ $version = *[!\ ]* ]]; 
 then 
   echo "Current version:" $version;
+  echo "Branch:" $WERCKER_GIT_BRANCH;
+  dest=${update}_${version}
+  echo "Target dir:" $dest;
   rm -rf tmp
   mkdir tmp && cd tmp
-  dest=update_$version
   mkdir $dest
   cp -r ../$dir/* $dest
-  ln -s $dest update
+  ln -s $dest $update
   rsync -a -e "ssh" -rtzh . $url:$remote
   exit $?;
 else
